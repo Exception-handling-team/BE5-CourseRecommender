aedb0793f160c5b23d521d36a947a0457c054fbd
wkdan <jmy8876@gmail.com>
2025-01-13 16:50:04 +0900

    test: cmd t2 r

--- src/test/java/app/global/CommandTest.java
+++ src/test/java/app/global/CommandTest.java
@@ -3,11 +3,22 @@ package app.global;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CommandTest {
 
     @Test
     @DisplayName("command Test 최초 테스트")
     void t1() {
         Command cmd = new Command("");
+    }
+
+    @Test
+    @DisplayName("action name 얻어오기, 삭제?id=1 입력시 삭제 출력")
+    void t2() {
+        Command cmd = new Command("삭제?id=1");
+        String actionName = cmd.getActionName();
+
+        assertThat(actionName).isEqualTo("삭제");
     }
 }
