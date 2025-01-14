3f32feb63bbcb0f8a1a0dd65d2f9cc5fbcda08f5
wkdan <jmy8876@gmail.com>
2025-01-14 17:39:11 +0900

    test: Util t2 r

--- src/test/java/app/standard/UtilTest.java
+++ src/test/java/app/standard/UtilTest.java
@@ -3,11 +3,28 @@ package app.standard;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UtilTest {
 
     @Test
     @DisplayName("최초 파일 테스트")
     void t1() {
         Util.File.test();
+    }
+
+    @Test
+    @DisplayName("파일 생성 - 빈 파일")
+    void t2() {
+
+        String file = "test.txt";
+
+        Util.File.createFile();
+
+        assertThat(Files.exists(Paths.get(file)))
+                .isTrue();
     }
 }
